4840ba88c13b0672e7e746e94eb94eaab9afab49
John Scott <john.scott17@nhs.net>
2022-10-11 17:03:23 +0100

    In the middle of dealing with Microsoft's silly database connection format, to finish tomorrow

--- R/db_r6.R
+++ R/db_r6.R
@@ -26,6 +26,18 @@ Database <- R6::R6Class(
       if (!file.exists(db_config))
         stop("Database connection failed, input credential file does not exist")
       private$config <- rjson::fromJSON(file = db_config)
+      print(private$config)
+      private$connection <- DBI::dbConnect(
+        driver = "SQL Server",
+        #RMariaDB::MariaDB(),
+        odbc::odbc(),
+        host = private$config$host,
+        port = private$config$port,
+        database = private$config$dbname,
+        #user = private$config$user,
+        #password = private$config$password
+      )
+      print(private$connection)
     }
 
 
@@ -38,5 +50,9 @@ Database <- R6::R6Class(
 # imports from R6 (see "https://stackoverflow.com/questions/64055049/
 # unexpected-note-namespace-in-imports-field-not-imported-from-r6)"
 #
+# I'm not really sure that this is the best way to do things, but it
+# gets rid of the check() notes for now.
+#
 #' @import R6
+#' @import rjson
 NULL
