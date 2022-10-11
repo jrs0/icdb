78122771e5b12214bff0aa5bd6a49aebb6f4ccf1
John Scott <john.scott17@nhs.net>
2022-10-11 16:25:47 +0100

    Added a json file for db credentials

--- R/db_r6.R
+++ R/db_r6.R
@@ -1,23 +1,42 @@
-Database <- R6::R6Class("Database",
-                        public = list(
-                          #' Title
-                          #'
-                          #' @param val A value to pass
-                          #'
-                          #' @return A new (R6) Database object
-                          #' @export
-                          #'
-                          #' @examples
-                          #'
-                          #' db <- Database$new(12)
-                          initialize = function(val)
-                          {
-                            private$val <- val
-                          }
+Database <- R6::R6Class(
+  "Database",
+  public = list(
+    #' Construct a Database object
+    #'
+    #' @param db_cred_file The absolute path of a configuration file (csv
+    #' format) containing database connection information and credentials.
+    #' The default value is the credential file stored in the inst/ directory
+    #'
+    #' @return A new (R6) Database object
+    #' @export
+    #'
+    #' @examples
+    #'
+    #' db <- Database$new()
+    initialize = function(db_cred_file = "db_cred_file.json")
+    {
+      if (!file.exists(db_cred_file))
+        stop("Database connection failed, input credential file does not exist")
+      json <- rjson::fromJSON(file = db_cred_file)
+      print(json)
+    }
 
 
-                        ),
-                        private = list(val = 10))
+  ),
+  private = list(
+    driver = NA_character_,
+    server = NA_character_,
+    host = NA_character_,
+    trusted_connection = NA,
+    user = NA_character_,
+    password = NA_character_,
+    name = NA_character_,
+    schema = NA_character_,
+    table = NA_character_,
+    port = NA_integer_,
+    connection = NA
+  )
+)
 
 
 # The lines below is necessary to surpress a warning about no
