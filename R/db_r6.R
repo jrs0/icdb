419c4522adbdee4a918f10a51a6ea213c6de4658
John Scott <john.scott17@nhs.net>
2022-10-11 15:43:50 +0100

    Fixed R6 import note in check()

--- R/db_r6.R
+++ R/db_r6.R
@@ -2,17 +2,27 @@ Database <- R6::R6Class("Database",
                         public = list(
                           #' Title
                           #'
-                          #' @param val
+                          #' @param val A value to pass
                           #'
-                          #' @return
+                          #' @return A new (R6) Database object
                           #' @export
                           #'
                           #' @examples
+                          #'
+                          #' db <- Database$new(12)
                           initialize = function(val)
                           {
-                            self$val <- val
+                            private$val <- val
                           }
 
 
                         ),
                         private = list(val = 10))
+
+
+# The lines below is necessary to surpress a warning about no
+# imports from R6 (see "https://stackoverflow.com/questions/64055049/
+# unexpected-note-namespace-in-imports-field-not-imported-from-r6)"
+#
+#' @import R6
+NULL
