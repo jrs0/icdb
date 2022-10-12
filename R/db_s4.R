628739c95ab2e39f2f2771de200b3ab7e2e01ef5
John Scott <john.scott17@nhs.net>
2022-10-12 13:46:08 +0100

    Tried adding some getter and setter methods (works)

--- R/db_s4.R
+++ R/db_s4.R
@@ -1,6 +1,26 @@
-setClass("Database",
+#' Database class wrapping an SQL server connection
+#'
+#' @slot connection Microsoft SQL Server.
+#' @slot config numeric.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+setClass("DatabaseS4",
             slots = c(
                 connection = "Microsoft SQL Server",
-                config = "numeric"
+                config = "numeric",
+                something = "numeric"
             )
 )
+
+setGeneric("something", function(x) {standardGeneric("something")})
+setGeneric("something<-", function(x, value) { standardGeneric("something<-")})
+
+setMethod("something", "DatabaseS4", function(x) { x@something })
+setMethod("something<-", "DatabaseS4", function(x, value)
+  {
+    x@something <- value
+    x
+  })
