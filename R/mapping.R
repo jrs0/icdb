77d93819e094f6adae0fc09e04c68da06c0fa5fd
John Scott <john.scott17@nhs.net>
2022-10-26 13:48:27 +0100

    Got a minimal working example of remapping database column names

--- R/mapping.R
+++ R/mapping.R
@@ -5,5 +5,54 @@ APC <- function(table)
     mapping <- yaml::read_yaml(system.file("extdata", "mapping.yaml", package = "icdb"))
 
     
-    table %>% dplyr::select(mapping$apc$episode$start) %>% head(10) %>% icdb::run()
+    table %>% dplyr::select(mapping$apc$episode$start) %>% head(10000) %>% icdb::run()
+}
+
+setClass(
+    "MappedDB",
+    contains = "list",
+    slots = representation(
+        mapping = "list"
+    ),
+    prototype = prototype(
+        mapping = list()
+    )    
+)
+
+
+logical_table_getter <- function(srv, database, source_table, logical_table)
+{
+    force(srv)
+    force(database)
+    force(source_table)
+    force(logical_table)
+    function()
+    {
+        srv[[database]][[source_table]]() %>%
+                                          dplyr::select(logical_table[[1]]) %>%
+                                          head(30) %>%
+                                          run()
+    }
+}
+
+MappedDB <- function(srv, mapping = system.file("extdata", "mapping.yaml", package="icdb"))
+{
+    m <- yaml::read_yaml(mapping)
+
+    mdb <- new("MappedDB", mapping = m)
+
+    # ldb stands for logical database
+    for (ldb_name in names(m))
+    {
+        ## Get the names of the source database and source table
+        database <- m[[ldb_name]]$database
+        source_table <- m[[ldb_name]]$source_table
+        
+        mdb[[ldb_name]] <- Tables()
+        mdb[[ldb_name]]@.Data <- m[[ldb_name]]$logical_tables %>%
+            purrr::map(~ logical_table_getter(srv, database, source_table, .x))
+        names(mdb[[ldb_name]]@.Data) <- names(m[[ldb_name]]$logical_tables)
+    }
+
+    mdb
 }
