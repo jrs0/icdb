38819cec2f7e62aafca49bcf21b92b7fb6f98709
John Scott <john.scott17@nhs.net>
2022-10-31 12:09:57 +0000

    Started parsing the acs codes file

--- R/apc.R
+++ R/apc.R
@@ -5,7 +5,8 @@ NULL
 spells <- function(tbl)
 {
     codes <- yaml::read_yaml(system.file("extdata", "acs_codes.yaml", package="icdb"))
-    tbl %>%
-        filter(primary_diagnosis %in% !!codes) %>%
-        dplyr::show_query
+    ## tbl %>%
+    ##     filter(primary_diagnosis %in% !!codes) %>%
+    ##     dplyr::show_query()
+    codes
 }
