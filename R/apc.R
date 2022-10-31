aa25aaa3e387d36b52248599c7bece67c90eff24
John Scott <john.scott17@nhs.net>
2022-10-31 12:24:09 +0000

    Started working on a function to extract ICD codes from the codes definition file

--- R/apc.R
+++ R/apc.R
@@ -2,11 +2,43 @@
 ##'
 NULL
 
+##' Get all the codes in a codes definition yaml file as a list.
+##'
+##' A codes definition file is a yaml with a tree-like structure
+##' where leaves of the tree are ICD codes, and non-leaf nodes
+##' are categories. Each level in the structure contains either
+##' a "categories" field (for a non-leaf) or a "code" field
+##' (for a leaf). All levels in the structure contains a "docs"
+##' field describing what the level means.
+##' 
+##' @title Get a flat list of codes
+##' @param codes The parsed codes definition list of lists
+##' @return A character vector containing the codes 
+##' @author 
+get_codes <- function(codes)
+{
+    if ("categories" %in% names(codes))
+    {
+        for (category in codes$categories)
+        {
+            get_codes(category)
+        }
+    }
+    else if ("code" %in% names(codes))
+    {
+        print(codes$code)
+    }
+    else
+    {
+        stop("Each level of the codes structure must contains 'categories' or 'code'")
+    }
+}
+
 spells <- function(tbl)
 {
     codes <- yaml::read_yaml(system.file("extdata", "acs_codes.yaml", package="icdb"))
     ## tbl %>%
     ##     filter(primary_diagnosis %in% !!codes) %>%
     ##     dplyr::show_query()
-    codes
+    get_codes(codes)
 }
