cfc16b24049ec84da6aa3ab87dc67a8f109d8d06
John Scott <john.scott17@nhs.net>
2022-10-31 17:03:07 +0000

    It seems that dplyr can generate pivot table queries

--- R/apc.R
+++ R/apc.R
@@ -2,9 +2,14 @@
 ##'
 NULL
 
-spells <- function(tbl)
+spells <- function(tbl, start = lubridate::ymd("2021-8-1"), end = lubridate::ymd("2021-8-10"))
 {
     codes <- yaml::read_yaml(system.file("extdata", "acs_codes.yaml", package="icdb"))
     tbl %>%
-        dplyr::filter(primary_diagnosis %in% !!get_codes(codes))
+        dplyr::filter(primary_diagnosis %in% !!get_codes(codes),
+                      start >= !!start,
+                      start <= !!end) %>%
+        tidyr::pivot_wider(names_from=primary_diagnosis, values_from=start) %>%
+        dplyr::show_query()
+        
 }
