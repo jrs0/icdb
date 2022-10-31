a650cae852fa3e1829af0c951c07fa6e652fd256
John Scott <john.scott17@nhs.net>
2022-10-31 12:54:24 +0000

    Need to strip trailing and leading whitespace from the ICD I tihnk

--- R/apc.R
+++ R/apc.R
@@ -2,40 +2,11 @@
 ##'
 NULL
 
-##' Get all the codes in a codes definition yaml file as a list.
-##'
-##' A codes definition file is a yaml with a tree-like structure
-##' where leaves of the tree are ICD codes, and non-leaf nodes
-##' are categories. Each level in the structure contains either
-##' a "categories" field (for a non-leaf) or a "code" field
-##' (for a leaf). All levels in the structure contains a "docs"
-##' field describing what the level means.
-##' 
-##' @title Get a flat list of codes
-##' @param codes The parsed codes definition list of lists
-##' @return A named character vector containing the codes 
-##' @author 
-get_codes <- function(codes, flat = character())
-{
-    if ("categories" %in% names(codes))
-    {
-        codes$categories %>%
-            purrr::map(~ get_codes(.)) %>%
-            unlist()
-    }
-    else if ("code" %in% names(codes))
-    {
-        codes$code
-    }
-    else
-    {
-        stop("Each level of the codes structure must contains 'categories' or 'code'")
-    }
-}
-
 spells <- function(tbl)
 {
     codes <- yaml::read_yaml(system.file("extdata", "acs_codes.yaml", package="icdb"))
     tbl %>%
-        dplyr::filter(primary_diagnosis %in% !!get_codes(codes))
+        dplyr::filter(primary_diagnosis %in% !!get_codes(codes)) %>% 
+        dplyr::select(primary_diagnosis) %>%
+        dplyr::distinct() %>% icdb::run()
 }
---
+++ R/codes.R
@@ -0,0 +1,34 @@
+##'
+##'
+NULL
+
+##' Get all the codes in a codes definition yaml file as a list.
+##'
+##' A codes definition file is a yaml with a tree-like structure
+##' where leaves of the tree are ICD codes, and non-leaf nodes
+##' are categories. Each level in the structure contains either
+##' a "categories" field (for a non-leaf) or a "code" field
+##' (for a leaf). All levels in the structure contains a "docs"
+##' field describing what the level means.
+##' 
+##' @title Get a flat list of codes
+##' @param codes The parsed codes definition list of lists
+##' @return A named character vector containing the codes 
+##' @author 
+get_codes <- function(codes, flat = character())
+{
+    if ("categories" %in% names(codes))
+    {
+        codes$categories %>%
+            purrr::map(~ get_codes(.)) %>%
+            unlist()
+    }
+    else if ("code" %in% names(codes))
+    {
+        codes$code
+    }
+    else
+    {
+        stop("Each level of the codes structure must contains 'categories' or 'code'")
+    }
+}
