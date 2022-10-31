6305b8f525520d21f27287867720113d2a817c11
John Scott <john.scott17@nhs.net>
2022-10-31 16:52:07 +0000

    Added the spell id field to the logical apc table

--- R/apc.R
+++ R/apc.R
@@ -6,7 +6,5 @@ spells <- function(tbl)
 {
     codes <- yaml::read_yaml(system.file("extdata", "acs_codes.yaml", package="icdb"))
     tbl %>%
-        dplyr::filter(primary_diagnosis %in% !!get_codes(codes)) %>% 
-        dplyr::select(primary_diagnosis) %>%
-        dplyr::distinct() %>% icdb::run()
+        dplyr::filter(primary_diagnosis %in% !!get_codes(codes))
 }
