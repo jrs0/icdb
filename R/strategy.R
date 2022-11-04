d1dd5e5d87f0f9a384e0444d5f6a90ffd7f10c01
John Scott <john.scott17@nhs.net>
2022-11-04 14:12:08 +0000

    Checked the flt condition in the query, appears to work

--- R/strategy.R
+++ R/strategy.R
@@ -112,7 +112,7 @@ strategy_codes_from <- function(tbl, name, codes_file)
     flt <- gen_filter(code_map, col)
     
     ## Perform the selection and filtering operation on the column
-    tbl %>% dplyr::filter(!!!flt) %>%
+    tbl %>% dplyr::filter(flt) %>%
         dplyr::mutate(!!name := case_when(!!!cases), .keep = "unused")
 }
 
