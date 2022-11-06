1f2f6d1469fbcb6d672451f862914bb1b63aa81b
John Scott <john.scott17@nhs.net>
2022-11-06 14:36:59 +0000

    Removed JSON configuration files from extdata

--- tests/testthat/test-srv.R
+++ tests/testthat/test-srv.R
@@ -5,21 +5,25 @@ test_that("Server throws an error when no connection information is provided", {
 })
 
 test_that("Server throws error when an non-existent config file is used", {
-    expect_error(Server(config="does_not_exist.json"))
+    expect_error(Server(config="does_not_exist.yaml"))
 })
 
 test_that("Server throws error when an invalid config file is used", {
-    expect_error(Server(config=system.file("extdata", "invalid.json",package="icdb")))
+    expect_error(Server(config=system.file("extdata", "invalid.yaml",package="icdb")))
 })
 
 test_that("Server throws error when a config file contains an invalid driver name", {
-    expect_error(Server(config=system.file("extdata", "bad_driver.json",package="icdb")))
+    expect_error(Server(config=system.file("extdata", "bad_driver.yaml",package="icdb")))
 })
 
 ### Tests for valid connections via config files
 
 test_that("Server connects to sqlite database without errors", {
-    srv <- Server(config=system.file("extdata", "sqlite.json", package="icdb"))
+
+    ## Generate test data (put the result in "gendata/test.db")
+    gen_apc("test.db")
+    
+    srv <- Server(config=system.file("extdata", "sqlite.yaml", package="icdb"))
     expect_true(DBI::dbIsValid(srv@con))
 })
 
