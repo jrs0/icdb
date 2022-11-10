ad2df87184c5e586818705abe7980cd691fa9332
John Scott <john.scott17@nhs.net>
2022-11-10 21:43:16 +0000

    Added a few more cache tests

--- tests/testthat/test-cache.R
+++ tests/testthat/test-cache.R
@@ -100,8 +100,96 @@ test_that("queries are flushed to the level 2 cache", {
                  dplyr::filter(hash %in% !!tbl1$hash, !in_memory) %>%
                  nrow(),
                  3)
+})
+
+### Check the cache lifetime mechanism
+
+test_that("check level 1 cache objects expires", {
+
+    ## Generate artificial data
+    gen_clean_apc("apc.db")
+
+    ## Connect to the database
+    srv <- server(config=system.file("extdata", "sqlite.yaml", package="icdb"))
+
+    ## Enable the cache, manually set a level 1 size
+    use_cache(TRUE, size = 2, lifetime = lubridate::dseconds(2))
+
+    ## Clear the cache (in case cache/ is present)
+    clear_cache()
+    expect_equal(nrow(show_cache()), 0)
+    
+    ## Run the same query three times and check the hits is four
+    for (n in 1:3)
+    {
+        srv$APC_SYNTH %>% head(1) %>% run()
+    }
+    tbl <- show_cache()
+    expect_equal(tbl$hits[[1]], 3)
+
+    ## Check that the cache object was last accessed at a
+    ## different time to when it was first written
+    expect_true(tbl$write_time[[1]] != tbl$last_access[[1]])
+    
+    ## Wait for 10 seconds so that the object expires
+    Sys.sleep(5)
+
+    ## Perform the same query again, expect hits to be equal to 1,
+    ## 
+    srv$APC_SYNTH %>% head(1) %>% run()
+    tbl <- show_cache()
+    expect_equal(tbl$hits[[1]], 1)
+
+    ## Check that the write time and last access are equal
+    expect_equal(tbl$write_time[[1]], tbl$last_access[[1]])
 
     
+})
+
+test_that("check level 2 cache objects expires", {
+
+    ## Generate artificial data
+    gen_clean_apc("apc.db")
+
+    ## Connect to the database
+    srv <- server(config=system.file("extdata", "sqlite.yaml", package="icdb"))
+
+    ## Enable the cache, manually set a level 1 size
+    use_cache(TRUE, size = 2, lifetime = lubridate::dseconds(2))
+
+    ## Clear the cache (in case cache/ is present)
+    clear_cache()
+    expect_equal(nrow(show_cache()), 0)
+    
+    ## Run multiple queries multiple times (the first
+    ## query will end up in the level 1 cache)
+    srv$APC_SYNTH %>% head(1) %>% run()
+    srv$APC_SYNTH %>% head(1) %>% run()
+    srv$APC_SYNTH %>% head(2) %>% run()
+    srv$APC_SYNTH %>% head(2) %>% run()
+    srv$APC_SYNTH %>% head(3) %>% run()
+    srv$APC_SYNTH %>% head(3) %>% run()
+    tbl <- show_cache()
+    expect_equal(tbl$hits[[1]], 2)
+
+    ## Check that the cache object was last accessed at a
+    ## different time to when it was first written
+    expect_true(tbl$write_time[[1]] != tbl$last_access[[1]])
+    
+    ## Wait for 10 seconds so that the object expires
+    Sys.sleep(5)
+
+    ## Perform the same query again, expect hits to be equal to 1,
+    ## 
+    srv$APC_SYNTH %>% head(1) %>% run()
+    tbl <- show_cache()
+    expect_equal(tbl$hits[[1]], 1)
+
+    ## Check that the write time and last access are equal
+    expect_equal(tbl$write_time[[1]], tbl$last_access[[1]])
+
     
 })
+
+
 
