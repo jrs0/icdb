ff43694ceec76926011ea6dff562a7609b9b771b
John Scott <john.scott17@nhs.net>
2022-11-10 10:20:45 +0000

    Started working on a function to parse the TOS

---
+++ R/gen_map.R
@@ -0,0 +1,16 @@
+##'
+##' 
+NULL
+
+##' .. content for \description{} (no empty lines) ..
+##'
+##' .. content for \details{} ..
+##' @title 
+##' @param tos 
+##' @param sheet 
+##' @return 
+##' @author 
+gen_map <- function(tos, sheet)
+{
+    readxl::read_xls("tos")
+}
