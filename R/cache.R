60fa2897f89bdde995b3a03f541d5e5dd985171f
John Scott <john.scott17@nhs.net>
2022-11-10 20:25:05 +0000

    Fixed bug with cache size, test passes

--- R/cache.R
+++ R/cache.R
@@ -17,7 +17,8 @@ pkg_env$cache <- list(
                                        time = as.difftime(1, units="hours") # How long did the original computation take
                                        ),
                   max_size = 5,
-                  objects = list()),
+                  objects = list()
+                  ),
     path = "cache/",
     use_cache = FALSE,
     lifetime = lubridate::dhours(24)
@@ -71,7 +72,7 @@ use_cache <- function(state, lifetime = lubridate::dhours(24), size = 5)
 {
     pkg_env$cache$use_cache <- state
     pkg_env$cache$lifetime <- lifetime
-    pkg_env$cache$max_size <- size
+    pkg_env$cache$level1$max_size <- size
 }
 
 record_hit <- function(metadata)
