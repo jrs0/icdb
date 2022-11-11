44f9c24cb28570b56a8d6586bfb3404df43128f6
John Scott <john.scott17@nhs.net>
2022-11-11 09:41:04 +0000

    Temporarily removed the filter from the codes_from strategy -- this should probably go at the next level up

--- R/strategy.R
+++ R/strategy.R
@@ -101,7 +101,7 @@ strategy_codes_from <- function(tbl, name, codes_files)
     flt <- gen_filter(code_map, col)
 
     ## Perform the selection and filtering operation on the column
-    tbl %>% dplyr::filter(flt) %>%
+    tbl %>% ##dplyr::filter(flt) %>%
         dplyr::mutate(!!name := case_when(!!!cases), .keep = "unused")
 }
 
