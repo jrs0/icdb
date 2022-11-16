074901ffef2c4755a85a8f40fe2deb80098c8b87
John Scott <john.scott17@nhs.net>
2022-11-16 13:03:04 +0000

    Added first GET request to API header with 200 return code

--- R/icd.R
+++ R/icd.R
@@ -1,29 +1,40 @@
-icd_apc <- function()
+icd_api <- function()
 {
-    conf <- yaml::read_yaml(system.file("extdata", "cred.secret.yaml", package = "icdb"))
-    url <- conf$token_endpoint
-    payload <- conf[-token_endpoint]
+    conf <- yaml::read_yaml(system.file("extdata", "icd-api.yaml", package = "icdb"))
+    ##url <- conf$token_endpoint
+    ##payload <- conf[-token_endpoint]
     
-    ## get the OAUTH2 token
-    r <- httr::POST(url = url, quer
+    ## ## get the OAUTH2 token
+    ## r <- httr::POST(url = url, quer
     
-    r = requests.post(token_endpoint, data=payload, verify=False).json()
-    token = r['access_token']
+    ## r = requests.post(token_endpoint, data=payload, verify=False).json()
+    ## token = r['access_token']
 
 
     ## access ICD API
 
-    uri = 'https://id.who.int/icd/entity'
+    url = 'http://localhost/icd/entity'
 
     ## HTTP header fields to set
-    headers = {'Authorization':  'Bearer '+token, 
-        'Accept': 'application/json', 
-        'Accept-Language': 'en',
-        'API-Version': 'v2'}
+    ## headers = {'Authorization':  'Bearer '+token, 
+    ##     'Accept': 'application/json', 
+    ##     'Accept-Language': 'en',
+    ##     'API-Version': 'v2'}
+    headers <- list("Accept" = "application/json",
+                    "Accept-Language" = "en",
+                    "API-Version" = "v2")
+
+    r <- httr::GET(url = url,
+                   httr::add_headers(
+                             `Accept` = "application/json",
+                             `Accept-Language` = "en",
+                             `API-Version` = "v2"
+                         )
+                   )
     
     ## make request           
-    r = requests.get(uri, headers=headers, verify=False)
+    #r = requests.get(uri, headers=headers, verify=False)
 
     ## print the result
-    print (r.text)
+    r
 }
