2e4e669b6ce098b6ff2ae90218c8d24a9ca1e542
John Scott <john.scott17@nhs.net>
2022-11-16 12:51:22 +0000

    Added the icd parsing function back in, got the docker ICD API working, going to develop the ICD parser

--- R/icd.R
+++ R/icd.R
@@ -1,30 +1,29 @@
-
-## icd_apc <- function()
-## {
-##     conf <- yaml::read_yaml(system.file("extdata", "cred.secret.yaml", package = "icdb"))
-##     url <- conf$token_endpoint
-##     payload <- conf[-token_endpoint]
+icd_apc <- function()
+{
+    conf <- yaml::read_yaml(system.file("extdata", "cred.secret.yaml", package = "icdb"))
+    url <- conf$token_endpoint
+    payload <- conf[-token_endpoint]
     
-##     ## get the OAUTH2 token
-##     r <- httr::POST(url = url, quer
+    ## get the OAUTH2 token
+    r <- httr::POST(url = url, quer
     
-##     r = requests.post(token_endpoint, data=payload, verify=False).json()
-##     token = r['access_token']
+    r = requests.post(token_endpoint, data=payload, verify=False).json()
+    token = r['access_token']
 
 
-##     ## access ICD API
+    ## access ICD API
 
-##     uri = 'https://id.who.int/icd/entity'
+    uri = 'https://id.who.int/icd/entity'
 
-##     ## HTTP header fields to set
-##     headers = {'Authorization':  'Bearer '+token, 
-##         'Accept': 'application/json', 
-##         'Accept-Language': 'en',
-##         'API-Version': 'v2'}
+    ## HTTP header fields to set
+    headers = {'Authorization':  'Bearer '+token, 
+        'Accept': 'application/json', 
+        'Accept-Language': 'en',
+        'API-Version': 'v2'}
     
-##     ## make request           
-##     r = requests.get(uri, headers=headers, verify=False)
+    ## make request           
+    r = requests.get(uri, headers=headers, verify=False)
 
-##     ## print the result
-##     print (r.text)
-## }
+    ## print the result
+    print (r.text)
+}
