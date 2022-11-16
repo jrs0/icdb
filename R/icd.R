0052c1a7b8a040ae71536258e463bad4a9d5611d
John Scott <john.scott17@nhs.net>
2022-11-16 13:17:21 +0000

    Not got a working ICD code GET yet

--- R/icd.R
+++ R/icd.R
@@ -13,24 +13,8 @@ icd_api <- function()
 
     ## access ICD API
 
-    url = 'http://localhost/icd/entity'
-
-    ## HTTP header fields to set
-    ## headers = {'Authorization':  'Bearer '+token, 
-    ##     'Accept': 'application/json', 
-    ##     'Accept-Language': 'en',
-    ##     'API-Version': 'v2'}
-    headers <- list("Accept" = "application/json",
-                    "Accept-Language" = "en",
-                    "API-Version" = "v2")
-
-    r <- httr::GET(url = url,
-                   httr::add_headers(
-                             `Accept` = "application/json",
-                             `Accept-Language` = "en",
-                             `API-Version` = "v2"
-                         )
-                   )
+    url = 'http://localhost/icd/release/10'
+    r <- httr::GET(url = url, httr::add_headers(`Accept` = "application/json",`Accept-Language` = "en",`API-Version` = "v2"))
     
     ## make request           
     #r = requests.get(uri, headers=headers, verify=False)
