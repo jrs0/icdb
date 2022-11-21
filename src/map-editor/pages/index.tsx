95d2f75802ebbfe02971f4101c41c67ba25a72e8
John Scott <john.scott17@nhs.net>
2022-11-21 22:43:49 +0000

    Got a simple minimal example of config file parsing using next

--- src/map-editor/pages/index.tsx
+++ src/map-editor/pages/index.tsx
@@ -1,12 +1,37 @@
-function Column(col) {
+function Column({ col }) {
     return (
-        col
+        <div>
+            <div>Name: {col.name}</div>
+            <div>Strategy: {col.strategy}</div>
+            <div>Use: {col.use}</div>
+            <div>Source columns</div>
+            <ul>
+                {col.source_columns.map((scol) => (
+                    <li>
+                        <div>Name: {scol.name}</div>
+                        <div>Docs: {scol.docs}</div>
+                    </li>
+                ))}
+            </ul>
+        </div>
     )
 }
 
-function Table(tab) {
+function Table({ tab }) {
     return (
-        tab
+        <div>
+            <div> Name: {tab.name} </div>
+            <div> Docs: {tab.docs} </div>
+            <div> Columns: </div>
+            <ul>
+                {tab.columns.map((col) => (
+                    <li>
+                        <Column col={col} />
+                    </li>
+                ))}
+            </ul>
+
+        </div>
     )
 }
 
@@ -17,52 +42,56 @@ export default function Home() {
             name: "apc",
             source: "ABI.dbo.vw_APC_SEM_001",
             docs: "The SUS database contains hospital episode statistics, which contain records of patient care activities that take place in a hospital visit.",
-            columns: {
-                nhs_number: {
+            columns: [
+                {
+                    name: "nhs_number",
                     docs: "The NHS number identifies the patient. The field is anonymised.",
                     source_columns: [
                         {
-                            "AIMTC_Pseudo_NHS": ""
+                            name: "AIMTC_Pseudo_NHS",
+                            docs: ""
                         },
                         {
-                            "NHSnumber": "This one looks like it is NULL everywhere. Also, this is an INT column"
+                            name: "NHSnumber",
+                            docs: "This one looks like it is NULL everywhere. Also, this is an INT column"
                         }
                     ],
                     strategy: "coalesce_exclude_null",
                     use: "yes"
                 }
-            }
+            ]
         },
         {
             name: "attr",
             docs: "The SWD attributes table contains records of patient attributes at the latest snapshot through the year (monthly updates), PLUS a few other measures such as 'air quality' that is NOT present in the activity history table (these additional measures is the only reason to keep a separate reference to this table, otherwise we could have just filter the activity history table by most recent).",
             source: "MODELLING_SQL_AREA.dbo.swd_attribute",
-            columns: {
-                nhs_number: {
+            columns: [
+                {
+                    name: "nhs_number",
                     docs: "The NHS number identifies the patient. The field is anonymised.",
                     source_columns: [
                         {
-                            "AIMTC_Pseudo_NHS": ""
+                            name: "AIMTC_Pseudo_NHS",
+                            docs: ""
                         },
                         {
-                            "NHSnumber": "This one looks like it is NULL everywhere. Also, this is an INT column"
+                            name: "NHSnumber",
+                            docs: "This one looks like it is NULL everywhere. Also, this is an INT column"
                         }
                     ],
                     strategy: "coalesce_exclude_null",
                     use: "yes"
                 }
-            }
+            ]
         },
     ]
-
 
     return (
         <div>
             <ul>
                 {def.map((tab) => (
-                    <li key={tab.name}>
-                        {tab.name}
-
+                    <li>
+                        <Table tab={tab} />
                     </li>
                 ))}
             </ul>
