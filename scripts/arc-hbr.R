ce2fab3faac186cc89c0256eed9f887db237a649
John Scott <john.scott17@nhs.net>
2022-11-24 12:01:14 +0000

    Excluded null nhs_number in the mapping, investigating an anomaly with the primary_diagnosis_icd between spells and episodes

--- scripts/arc-hbr.R
+++ scripts/arc-hbr.R
@@ -5,4 +5,21 @@ library(tidyverse)
 library(tidymodels)
 library(lubridate)
 
+use_cache(TRUE, size = 1)
+
 msrv <- mapped_server("xsw")
+
+## Start and end dates for index percutaneous coronary intervention (PCI)
+## procedures. This script will use acute coronary syndrome (ACS)
+## diagnosis codes in lieu of procedure codes for now.
+start <- ymd("2000-1-1")
+end <- today()
+
+## Subtract one year from the end to get the date of
+## the last valid index event (where there is one complete
+## follow-up year)
+index_end <- end - days(365)
+
+spells <- msrv$sus$apc_spells %>%
+    filter(spell_start >= !!start, spell_end <= !!index_end) %>%
+    run()
