90810e65d39927712ad27022e9bdc89c76be810e
John Scott <john.scott17@nhs.net>
2022-11-25 17:09:58 +0000

    Added the Z380 code to the test cases

--- tests/testthat/test-icd.R
+++ tests/testthat/test-icd.R
@@ -1,3 +1,9 @@
-test_that("A selection of ICD-10 codes that have previously failed to parse", {
+test_that("a selection of ICD-10 codes that have previously failed to parse", {
   expect_equal(format(icd10("A000")), "A00.0")
+  expect_equal(format(icd10("Z380")), "Z38.0")
+
+})
+
+test_that("a selection of invalid ICD-10 codes throw errors", {
+  expect_error(icd10("I222")) ## There is no I22.2
 })
