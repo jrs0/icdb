23f3ddd0245525762d8b4217b696a2c95619dbff
John Scott <john.scott17@nhs.net>
2022-11-25 16:09:05 +0000

    Found the root cause -- coalesce is in icdb. Added a comment todo later.

--- R/strategy.R
+++ R/strategy.R
@@ -66,6 +66,10 @@ coalesce <- function(tbl, name)
 ##' @return The tbl after reducing
 coalesce_exclude_null <- function(tbl, name)
 {
+    ## This coalesce is not from dplyr, it is from icdb
+    ## (the function above). Cannot put icdb:: in front
+    ## because it is not exported. Feels like there should be
+    ## a way to specify it, but TODO later...
     tbl %>% coalesce(name) %>%
         dplyr::filter(!is.na(!!as.name(name)))
 }
