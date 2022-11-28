cc52b7f3a267640461878e1d01d184e563bf914f
John Scott <john.scott17@nhs.net>
2022-11-28 11:51:28 +0000

    Figured out how to usse lag with fill to get the time to the closest subsequent row matching condition

--- scripts/arc-hbr.R
+++ scripts/arc-hbr.R
@@ -32,18 +32,27 @@ spells <- msrv$sus$apc_spells %>%
     codes_from(c("icd10/acs.yaml", "icd10/bleeding.yaml"), primary_diagnosis_icd) %>% 
     run()
 
-## Get 
+## Get sequences of spells that began with an ACS event and contained
+## at least one subsequent ACS or bleeding event.
 subsequent <- spells %>%
     ## Add a column "type" that contains either "acs" or "bleeding"
     mutate(type = drop_detail(primary_diagnosis_icd, 1)) %>% 
     ## Group by patient
     group_by(nhs_number) %>%
-    ## Arrange in order of episode start and event type
+    ## Arrange in order of spell start and event type
     arrange(spell_start, type, .by_group = TRUE) %>%
     ## Only keep those groups which started with an ACS event
     filter(first(type) == "acs") %>%
     ## Only keep those groups that also contain a subsequent bleeding event
-    filter(any(type == "bleeding"))# %>%
+    filter(any(type == "bleeding"))## %>%
     ## Only keep groups where the bleeding even is within the post window
     ##filter(nth(spell_start, 2) < first(spell_start) - post)
 
+## Find ACS events that were followed by bleeding events
+## within the post-index window. 
+within_post <- subsequent %>%
+    ## For each ACS event, calculate the time to the nearest
+    ## subsequent bleeding event
+    mutate(val = if_else(type == "bleeding", spell_start, NULL)) %>%
+    fill(val, .direction = "up") %>%
+    mutate(next_bleeding = val - spell_start)
