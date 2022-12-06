a05494b09f9cf1614560664b6e602050f12dd98e
John Scott <john.scott17@nhs.net>
2022-12-06 11:03:12 +0000

    Going to just cut to the chase and implement new_icd10 in c++ -- profiling shows that 95% of the time is icd10_str_to_indices, even with the cache, so getting this faster will hopefully make a big difference.

--- scripts/arc-hbr.R
+++ scripts/arc-hbr.R
@@ -35,7 +35,7 @@ all_spells <- msrv$sus$apc_spells %>%
 ## Path to codes file
 code_file <- system.file("extdata/icd10/icd10_arc.yaml", package="icdb")
 
-spells <- all_spells[1:5000,] %>%
+spells <- all_spells[1:100000,] %>%
     mutate(p = icd10(primary_diagnosis_icd, code_file))
 
     
