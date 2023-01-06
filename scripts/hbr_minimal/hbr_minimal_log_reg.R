1585ddfaa5a1decf2d3ea954393725b8a33e0a39
John Scott <john.scott17@nhs.net>
2023-01-06 12:15:08 +0000

    Found convergence problems with logistic regression in minimal hbr dataset

--- scripts/hbr_minimal/hbr_minimal_log_reg.R
+++ scripts/hbr_minimal/hbr_minimal_log_reg.R
@@ -5,18 +5,55 @@
 
 library(tidyverse)
 library(caret)
+library(corrplot)
 
 hbr_minimal_dataset <- readRDS("gendata/hbr_minimal_dataset.rds")
 
-ctrl <- trainControl(summaryFunction = twoClassSummary,
-                     classProbs = TRUE)
-
-levels(hbr_minimal_dataset$bleed)
-
 ## Drop the date from the predictors
-dataset <- hbr_minimal_dataset %>%
+without_date <- hbr_minimal_dataset %>%
     select(-date)
 
+predictors <- without_date %>%
+    select(-bleed)
+
+## Remove zero variance predictors
+near_zero_indices <- nearZeroVar(predictors)
+predictors <- predictors %>%
+    select(-near_zero_indices)
+
+## Show the remaining columns
+predictors %>% colnames()
+
+## ## Encode the remaining factor columns as dummy variables
+## dummy_mod <- dummyVars(~ ., data = predictors)
+## dummy_predictors <- predict(dummy_mod, predictors)
+## dummy_predictors <- as.data.frame(dummy_predictors)
+## dummy_predictors <- as_tibble(dummy_predictors)
+
+## dataset$age <- without_date$age
+## dataset$bleed <- without_date$bleed
+
+## View correlations in the predictors
+## cor <- cor(predictors)
+## corrplot(cor)
+
+## Remove zero variance
+
+## Do the logistic regression
+ctrl <- trainControl(summaryFunction = twoClassSummary,
+                     classProbs = TRUE)
+set.seed(476)
+lr_full <- train(dataset,
+                 y = dataset$bleed,
+                 method = "glm",
+                 metric = "ROC",
+                 trControl = ctrl)
+
+
+
+
+
+## Stuff
 model_fit <- glm(bleed ~ .,
                  ## Select the rows for the pre-2008 data:
                  data = dataset,
