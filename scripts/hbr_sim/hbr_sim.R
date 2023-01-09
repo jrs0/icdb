efcb53fd7e7014ddae7b0a6eae6eaa0f991d212b
John Scott <john.scott17@nhs.net>
2023-01-09 09:52:35 +0000

    Created dataset of major criteria

--- scripts/hbr_sim/hbr_sim.R
+++ scripts/hbr_sim/hbr_sim.R
@@ -16,8 +16,56 @@ library(tidyverse)
 ## Total number of patients
 n <- 10000
 
-## Proportion of population at high bleeding risk
+## Proportion of population at high bleeding risk (HBR)
 p_hbr <- 0.444
 
+## Proportion of various major criteria, given HBR
+p_anemia_hbr <- 0.332
+p_oac_hbr <- 0.185
+p_malignancy_hbr <- 0.168
+p_ckd_hbr <- 0.137
+p_surgery_hbr <- 0.082
+
 set.seed(1023)
-hbr <- rbinom(n = n, size = 1, prob = p_hbr)
+
+## Randomly choose the rows at HBR
+hbr <- tibble(hbr = rbinom(n = n, size = 1, prob = p_hbr))
+
+## Of the HBR rows, randomly generate major criteria according
+## to the proportions above. For non-HBR rows, give no major
+## critera
+hbr <- hbr %>%
+    mutate(anemia = rbinom(n=n, size=hbr, p=p_anemia_hbr)) %>%
+    mutate(oac = rbinom(n=n, size=hbr, p=p_oac_hbr)) %>%
+    mutate(malignancy = rbinom(n=n, size=hbr, p=p_malignancy_hbr)) %>%
+    mutate(ckd = rbinom(n=n, size=hbr, p=p_ckd_hbr)) %>%
+    mutate(surgery = rbinom(n=n, size=hbr, p=p_surgery_hbr))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
