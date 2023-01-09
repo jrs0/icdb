00dbcb1f5bbfa1f4e325fd2b5f949372c2035089
John Scott <john.scott17@nhs.net>
2023-01-09 12:46:28 +0000

    Trying to use age as the only predictor -- not working

--- scripts/hbr_sim/hbr_sim.R
+++ scripts/hbr_sim/hbr_sim.R
@@ -15,6 +15,7 @@ library(tidyverse)
 library(corrplot)
 library(caret)
 library(pROC)
+library(ggplot2)
 
 ## Total number of patients
 n <- 10000
@@ -37,71 +38,45 @@ p_bleed_hbr <- 0.091
 
 set.seed(1023)
 
-## Randomly choose the rows at HBR
-hbr <- tibble(hbr = rbinom(n = n, size = 1, prob = p_hbr))
+## Generate normally distributed ages
+hbr <- tibble(age = rnorm(n = n, mean = 75, sd = 7))
 
-## Of the HBR rows, randomly generate major criteria according
-## to the proportions above. For non-HBR rows, give no major
-## critera
+## Generate HBR based deterministically on age
 hbr <- hbr %>%
-    mutate(anemia = rbinom(n=n, size=1, p=hbr*p_anemia_hbr)) %>%
-    mutate(oac = rbinom(n=n, size=1, p=hbr*p_oac_hbr)) %>%
-    mutate(malignancy = rbinom(n=n, size=1, p=hbr*p_malignancy_hbr)) %>%
-    mutate(ckd = rbinom(n=n, size=1, p=hbr*p_ckd_hbr)) %>%
-    mutate(surgery = rbinom(n=n, size=1, p=hbr*p_surgery_hbr))
+    mutate(hbr = if_else(age >= 75, 1, 0))
+
+## Add noise, so that HBR does not depend completely
+## deterministically on age. This models a more
+## realistic scenario, where there are other hidden
+## variables (determinsitically) affecting bleeding
+## risk
+random_hbr <- rbinom(n = n, size = 1, prob = 0.05)
+hbr <- hbr %>% mutate(hbr = xor(hbr, random_hbr))
+
+## Convert to factor
+hbr$hbr <- factor(hbr$hbr)
+levels(hbr$hbr) <- c("not_hbr", "is_hbr")
+
+## Plot age distribution
+ggplot(hbr, aes(x=age, fill=hbr)) + geom_density(alpha = 0.3)
 
 ## Generate bleed occurance based on bleeding risk
 hbr <- hbr %>%
-    mutate(bleeding_risk = if_else(hbr == 0, p_bleed_base, p_bleed_hbr)) %>%
+    mutate(bleeding_risk = if_else(hbr == "not_hbr", p_bleed_base, p_bleed_hbr)) %>%
     mutate(bleed_occured = rbinom(n = n, size = 1, p = bleeding_risk))
-
-## Show correlations. Note the low correlation between bleeding risk and
-## whether a bleed occured, due purely to the proportions involved.
-cc <-cor(hbr)
-corrplot(cc)
-
-## Check the proportions of major criteria in the HBR group
-## (should agree with the numbers above)
-print("In the HBR group")
-hbr %>% filter(hbr == 1) %>% summary()
-print("In the non-HBR group")
-hbr %>% filter(hbr == 0) %>% summary()
 
 ## Convert columns to factors
 hbr$bleed_occured <- factor(hbr$bleed_occured)
 levels(hbr$bleed_occured) <- c("no_bleed", "bleed_occured")
-hbr$hbr <- factor(hbr$hbr)
-levels(hbr$hbr) <- c("not_hbr", "is_hbr")
-
-
-hbr_fact$bleed_occured <- factor(hbr$bleed_occured, levels=c("no_bleed", "bleed_occured"))
-
-mutate(bleed_occured = )
-levels(hbr_fact$bleed_occured) <- c("no_bleed", "bleed_occured")
-hbr_fact <- hbr %>%
-    mutate(bleed_occured = factor(bleed_occured))
-levels(hbr_fact$bleed_occured) <- c("no_bleed", "bleed_occured")
-
 
 ## In this dataset, The HBR column is assumed to reflect the truth
 ## about who is at high bleeding risk. Being at high bleeding
 ## risk is then modelled as having a raised probability of bleeding
 ## from the baseline 3.2% to the higher risk 9.1%. Conceptually,
 ## bleeding risk depends deterministically on patient characteristics,
-## if enough characteristics were accounted for. In reality, it is not
-## possible to realise the determinsitic mapping between patient
-## characteristics and bleeding risk. By choosing a restricted set
-## of input predictors, risk will no longer depend deterministically
-## on the predictors. Depending on the quality of the predictors, an
-## how well they really map to the causes of bleeding risk, the
-## dependence of bleeding risk on the predictors will be more or
-## less deterministic
-
-## In this simulated dataset, the assumed true bleeding risk is
-## contained in the bleeding_risk column (or the HBR column). It
-## takes only two values: an HBR value; and a non-HBR value. These
-## values may be predictable from patient characteristics, if they
-## depend sufficiently determinstically on them.
+## if enough characteristics were accounted for. Here, this is modelled
+## by having bleeding risk depend exactly on age -- if age >= 75, then
+## the is_hbr flag is set.
 
 ## First, use logistic regression to predict high-bleeding risk
 ## from the input predictors.
@@ -112,7 +87,7 @@ ctrl <- trainControl(summaryFunction = twoClassSummary,
                      number = 10,
                      classProbs = TRUE,
                      savePredictions = TRUE)
-lr_hbr <- train(hbr[,2:6], y = hbr$hbr,
+lr_hbr <- train(hbr[,1], y = hbr$hbr,
                 method = "glm",
                 metric = "ROC",
                 trControl = ctrl)
@@ -129,20 +104,58 @@ hbr$lr_hbr_prob <- lr_hbr_pred_prob[,"is_hbr"]
 ## Plot the ROC curve for the 
 roc_curve <- roc(response = hbr$hbr,
                  predictor = hbr$lr_hbr_prob)
-
-## Show the area under the ROC curve with a confidence interval
 auc(roc_curve)
 ci(roc_curve)
-
-## Plot the ROC curve
 plot(roc_curve, legacy.axes = TRUE)
 
-## Predict the class from the LR model
-dataset$lr_bleed <- predict(lr_full, new_data = dataset)
+## Predict the bleeding risk (HBR or not-HBR) from the
+## lr_hbr model, and plot the confusion matrix
+hbr$lr_hbr_pred <- predict(lr_hbr, new_data = hbr)
+confusionMatrix(hbr$lr_hbr_pred, hbr$hbr)
 
-## Plot the confusion matrix 
-confusionMatrix(prediction, dataset$bleed)
+## Now repeat the analysis attempting to predict bleeding. It
+## is not possible to predict bleeding, because it is a random
+## event (the probability ranges from 3% - 10%). It is also
+## not possible to use bleeding events as a proxy for bleeding
+## risk, because the difference between the probabilities for
+## HBR and non-HBR is too low to distinguish in this sample size
+lr_bleed <- train(hbr[,1], y = hbr$bleed_occured,
+                method = "glm",
+                metric = "ROC",
+                trControl = ctrl)
 
+## View the summary, look for ROC area, which is the average
+## over the n folds of the cross validation. The paper states
+## that 
+lr_bleed
+
+## Predict the class probabilities and add the probability of
+lr_bleed_pred_prob <- predict(lr_bleed, new_data = hbr, type = "prob")
+hbr$lr_bleed_prob <- lr_bleed_pred_prob[,"bleed_occured"]
+
+## Plot the ROC curve for the 
+roc_curve <- roc(response = hbr$bleed_occured,
+                 predictor = hbr$lr_bleed_prob)
+auc(roc_curve)
+ci(roc_curve)
+plot(roc_curve, legacy.axes = TRUE)
+
+## Attempt to predict bleeding events from the
+## model. This model predicts all events as non
+## bleeds.
+hbr$lr_bleed_pred <- predict(lr_bleed, new_data = hbr)
+confusionMatrix(hbr$lr_bleed_pred, hbr$bleed_occured)
+
+## The real question is what is the value of predicting
+## bleeding as a proxy for predicting HBR, the real
+## quantity of interest
+roc_curve <- roc(response = hbr$hbr,
+                 predictor = hbr$lr_bleed_prob)
+auc(roc_curve)
+ci(roc_curve)
+plot(roc_curve, legacy.axes = TRUE)
+
+table(hbr$hbr, hbr$bleed_occured)
 
 
 
