c808337faa854b9c9f87ab4b1931975715abcf55
John Scott <john.scott17@nhs.net>
2023-02-09 15:21:35 +0000

    Added similar model for ischaemia

--- scripts/risk_tradeoff_minimal/risk_tradeoff_minimal_log_reg.R
+++ scripts/risk_tradeoff_minimal/risk_tradeoff_minimal_log_reg.R
@@ -10,50 +10,99 @@ library(tidymodels)
 ## all the ICD <code>_before columns, and a response variable bleed.
 risk_tradeoff_minimal_dataset <- readRDS("gendata/risk_tradeoff_minimal_dataset.rds")
 dataset <- risk_tradeoff_minimal_dataset %>%
-    mutate(bleed_after = factor(bleed_after == 0, labels = c("bleed_occured", "no_bleed")))
+    mutate(bleed_after = factor(bleed_after == 0, labels = c("bleed_occured", "no_bleed"))) %>%
+    mutate(ischaemia_after = factor(ischaemia_after == 0, labels = c("ischaemia_occured", "no_ischaemia")))
 
 set.seed(47)
 
+## ======= Bleeding model =======
+
 ## Create the training and test data
-dataset_split <- initial_split(dataset, prop = 0.75,
+bleeding_split <- initial_split(dataset, prop = 0.75,
                                strata = bleed_after)
-dataset_train <- training(dataset_split)
-dataset_test <- testing(dataset_split)
+bleeding_train <- training(bleeding_split)
+bleeding_test <- testing(bleeding_split)
 
 ## Logistic regression requires preprocessing of the predictors
 ## for sparse/unbalanced variables (p. 285, APM).
-dataset_rec <- recipe(bleed_after ~ ., data = dataset_train) %>%
+bleeding_rec <- recipe(bleed_after ~ ., data = bleeding_train) %>%
     update_role(date, new_role = "date") %>%
     update_role(ischaemia_after, new_role = "ischaemic_after") %>%
     step_integer(stemi_presentation) %>%
     step_nzv(all_predictors()) %>%
     step_center(all_predictors()) %>%
     step_scale(all_predictors())    
-summary(dataset_rec)
+summary(bleeding_rec)
 
 ## Specify a logistic regression model
 lr_model <- logistic_reg() %>% 
     set_engine('glm') %>% 
     set_mode('classification')
 
-dataset_workflow <- 
+bleeding_workflow <- 
     workflow() %>% 
     add_model(lr_model) %>% 
-    add_recipe(dataset_rec)
+    add_recipe(bleeding_rec)
 
 ## Fit to training data
-dataset_fit <- dataset_workflow %>%
-    fit(data = dataset_train)
+bleeding_fit <- bleeding_workflow %>%
+    fit(data = bleeding_train)
 
 ## View the fit
-dataset_fit %>%
+bleeding_fit %>%
     extract_fit_parsnip() %>% 
     tidy()
 
-## Predict using the test dataset
-dataset_aug <- augment(dataset_fit, dataset_test)
+## Predict using the test bleeding
+bleeding_aug <- augment(bleeding_fit, bleeding_test)
 
 ## Plot the ROC curve
-dataset_aug %>% 
+bleeding_aug %>% 
   roc_curve(truth = bleed_after, .pred_bleed_occured) %>% 
   autoplot()
+
+## ======= Ischaemia model =======
+
+## Create the training and test data
+ischaemia_split <- initial_split(dataset, prop = 0.75,
+                                 strata = ischaemia_after)
+ischaemia_train <- training(ischaemia_split)
+ischaemia_test <- testing(ischaemia_split)
+
+## Logistic regression requires preprocessing of the predictors
+## for sparse/unbalanced variables (p. 285, APM).
+ischaemia_rec <- recipe(ischaemia_after ~ ., data = ischaemia_train) %>%
+    update_role(date, new_role = "date") %>%
+    update_role(bleed_after, new_role = "bleed_after") %>%
+    step_integer(stemi_presentation) %>%
+    step_nzv(all_predictors()) %>%
+    step_center(all_predictors()) %>%
+    step_scale(all_predictors())    
+summary(ischaemia_rec)
+
+## Specify a logistic regression model
+lr_model <- logistic_reg() %>% 
+    set_engine('glm') %>% 
+    set_mode('classification')
+
+ischaemia_workflow <- 
+    workflow() %>% 
+    add_model(lr_model) %>% 
+    add_recipe(ischaemia_rec)
+
+## Fit to training data
+ischaemia_fit <- ischaemia_workflow %>%
+    fit(data = ischaemia_train)
+
+## View the fit
+ischaemia_fit %>%
+    extract_fit_parsnip() %>% 
+    tidy()
+
+## Predict using the test ischaemia
+ischaemia_aug <- augment(ischaemia_fit, ischaemia_test)
+
+## Plot the ROC curve
+ischaemia_aug %>% 
+    roc_curve(truth = ischaemia_after, .pred_ischaemia_occured) %>% 
+    autoplot()
