0c095854844feb00a519293f58cb65d659f9bec7
John Scott <john.scott17@nhs.net>
2023-02-23 13:55:44 +0000

    Managed to store the fits for each bootstrap

--- scripts/risk_tradeoff_minimal/log_reg.R
+++ scripts/risk_tradeoff_minimal/log_reg.R
@@ -24,85 +24,85 @@ summary(dataset)
 set.seed(47)
 
 
-sample_and_fit <- function(sample_num)
-{
+## ======== Test/train split ===========
 
-    ## ======== Test/train split ===========
+## Get a test training split stratified by bleeding (the
+## less common outcome)
+split <- initial_split(dataset, prop = 0.75, strata = bleed_after)
+train <- training(split)
+test <- testing(split) %>%
+    ## The id is necessary later to pair up bleeding/ischaemic predictions
+    mutate(id = as.factor(row_number()))
 
-    ## Get a test training split stratified by bleeding (the
-    ## less common outcome)
-    split <- initial_split(dataset, prop = 0.75, strata = bleed_after)
-    train <- training(split)
-    test <- testing(split) %>%
-        ## The id is necessary later to pair up bleeding/ischaemic predictions
-        mutate(id = row_number())
+## Create cross-validation folds
+folds <- vfold_cv(train,
+                  v = 10,
+                  strata = bleed_after)
 
-    ## Create cross-validation folds
-    folds <- vfold_cv(train, v = 10)
+## ========= Model selection =============
 
-    ## ========= Model selection =============
+model <- logistic_reg() %>% 
+    set_engine('glm') %>% 
+    set_mode('classification')
 
-    model <- logistic_reg() %>% 
-        set_engine('glm') %>% 
-        set_mode('classification')
+## ========= Recipes ============
 
-    ## ========= Recipes ============
+recipes = list(
+    ## Logistic regression requires preprocessing of the predictors
+    ## for sparse/unbalanced variables (p. 285, APM).
+    bleed = recipe(bleed_after ~ ., data = train) %>%
+        update_role(date, new_role = "date") %>%
+        update_role(ischaemia_after, new_role = "ischaemic_after") %>%
+        step_integer(stemi_presentation) %>%
+        step_nzv(all_predictors()) %>%
+        step_center(all_predictors()) %>%
+        step_scale(all_predictors()),
+    ## Logistic regression requires preprocessing of the predictors
+    ## for sparse/unbalanced variables (p. 285, APM).
+    ischaemia = recipe(ischaemia_after ~ ., data = train) %>%
+        update_role(date, new_role = "date") %>%
+        update_role(bleed_after, new_role = "bleed_after") %>%
+        step_integer(stemi_presentation) %>%
+        step_nzv(all_predictors()) %>%
+        step_center(all_predictors()) %>%
+        step_scale(all_predictors()))
 
-    recipes = list(
-        ## Logistic regression requires preprocessing of the predictors
-        ## for sparse/unbalanced variables (p. 285, APM).
-        bleed = recipe(bleed_after ~ ., data = train) %>%
-            update_role(date, new_role = "date") %>%
-            update_role(ischaemia_after, new_role = "ischaemic_after") %>%
-            step_integer(stemi_presentation) %>%
-            step_nzv(all_predictors()) %>%
-            step_center(all_predictors()) %>%
-            step_scale(all_predictors()),
-        ## Logistic regression requires preprocessing of the predictors
-        ## for sparse/unbalanced variables (p. 285, APM).
-        ischaemia = recipe(ischaemia_after ~ ., data = train) %>%
-            update_role(date, new_role = "date") %>%
-            update_role(bleed_after, new_role = "bleed_after") %>%
-            step_integer(stemi_presentation) %>%
-            step_nzv(all_predictors()) %>%
-            step_center(all_predictors()) %>%
-            step_scale(all_predictors()))
+## ========= Workflows ==========
 
-    ## ========= Workflows ==========
+## Each models (from the models list) has two recipes associated
+## with it -- one for bleeding and one for ischaemia prediction
+base_workflow <- workflow() %>%
+    add_model(model)
+workflows <- list(
+    bleed = base_workflow %>%
+        add_recipe(recipes$bleed),
+    ischaemia = base_workflow %>%
+        add_recipe(recipes$ischaemia))
 
-    ## Each models (from the models list) has two recipes associated
-    ## with it -- one for bleeding and one for ischaemia prediction
-    base_workflow <- workflow() %>%
-        add_model(model)
-    workflows <- list(
-        bleed = base_workflow %>%
-            add_recipe(recipes$bleed),
-        ischaemia = base_workflow %>%
-            add_recipe(recipes$ischaemia))
+## ========= Fit the models =========
 
-    ## ========= Fit the models =========
+ctrl_rs <- control_resamples(
+    extract = function (x) extract_fit_parsnip(x))
 
-    fit <- list(
-        bleed = workflows$bleed %>%
-            fit(data = train),
-        ischaemia = workflows$ischaemia %>%
-            fit(data = train))
-
-    pred_bleed <- fit$bleed %>% augment(test)
-    pred_ischaemia <- fit$ischaemia %>% augment(test)
-    
-    ## Predict using the test set. Data is in wide format,
-    ## with the bleeding and ischaemia predictions
-    pred_bleed %>%
-        left_join(pred_ischaemia, by=c("id"="id"))    
-}
+fit_rs <- list(
+    bleed = workflows$bleed %>%
+        fit_resamples(folds, control = ctrl_rs),
+    ischaemia = workflows$ischaemia %>%
+        fit_resamples(folds, control = ctrl_rs))
 
 
-pred <- 1:12 %>%
-    purrr::map(~ sample_and_fit(.x)) %>%
-    purrr::list_rbind() %>%
-    mutate(id = as.factor(id))
+pred_bleed <- fit$bleed %>% augment(test)
+pred_ischaemia <- fit$ischaemia %>% augment(test)
 
+## Predict using the test set. Data is in wide format,
+## with the bleeding and ischaemia predictions
+pred <- pred_bleed %>%
+    left_join(pred_ischaemia, by=c("id"="id"))
+
+
+## pred <- 1:12 %>%
+##     purrr::map(~ sample_and_fit(.x)) %>%
+##     purrr::list_rbind()
 
 pred %>%
     ## Uncomment to view one model for all patients
