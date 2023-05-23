c176807159d35538ef2040dcd60f8d512960eab0
John Scott <john.scott17@nhs.net>
2023-05-23 15:54:51 +0100

    Added a test script looking at the swd tables

--- scripts/swd_test.R
+++ scripts/swd_test.R
@@ -1,5 +1,19 @@
 ## Load icdb before running this script
+library(lubridate)
+library(tidyverse)
 
 use_cache(TRUE, lifetime = ddays(30))
 
 msrv <- mapped_server("xsw")
+
+msrv$swd$swd_attribute %>%
+    select(nhs_number) %>%
+    run() %>%
+    drop_na() %>%
+    nrow()
+
+msrv$swd$attr_h %>%
+    select(nhs_number) %>%
+    run() %>%
+    nrow()
+    
